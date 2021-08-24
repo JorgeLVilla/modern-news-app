8dc6bbfa8ae5e9ac8fe1e61f93103c91f7436a26
Your Name <you@example.com>
2021-08-24 14:49:44 -0700

    got rid of console.log

--- modern-news-app/src/util/fetchApi.ts
+++ modern-news-app/src/util/fetchApi.ts
@@ -11,7 +11,6 @@ export const fetchNewsData = async (searchQuery: string) => {
     try {
         const baseUrl = `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}${pageSize}${pageNumber}${sortBy}`;
         return await fetch(baseUrl).then(res => res.json()).then(data => {
-                console.log(data)
             }
         );
         
