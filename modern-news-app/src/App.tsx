190c1b5e4d07f9068d49df4051dc6ff1a276909a
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-08-24 17:35:50 -0700

    feat(app): implement fetchapi with typescript interface, add articles.ts file with interface

--- modern-news-app/src/App.tsx
+++ modern-news-app/src/App.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { fetchNewsData } from './util/fetchApi';
+import { EverythingNewsRequest } from './models/Articles';
 
 function App() {
+
+  const getNewsData = async () => {
+    try {
+      const request = new EverythingNewsRequest({q: "corona"});
+      const dataObject = await fetchNewsData(request);
+      return dataObject;
+    } catch (error) {
+      
+    }
+  }
+
+  getNewsData();
+  
   return (
     <div className="App">
       hi
