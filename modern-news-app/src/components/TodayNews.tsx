dfc5e747b266240d984020bf4ad02d589af1b97b
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-08-27 16:56:24 -0700

    feat(todaynews): resize article and heading containers

--- modern-news-app/src/components/TodayNews.tsx
+++ modern-news-app/src/components/TodayNews.tsx
@@ -1,5 +1,87 @@
 import React, { useState, useEffect } from 'react';
-import { ITodayNewsResponse } from '../models/TodayArticles'
+import { ITodayNewsResponse } from '../models/TodayArticles';
+import styled, { css } from 'styled-components';
+
+
+// styled component variables
+const TodayNewsSection = styled.div`
+    border: 2px solid red;
+
+    & .FeaturedArticle {
+        margin: 50px auto;
+        width: 10%;
+        height: auto;
+        border: 3px solid green;
+        padding: 30px;
+        display: flex;
+        flex-direction: row;
+        background-color: red;
+    }
+
+    & .StandardArticle {
+        width: 350px;
+    }
+`;
+
+    export const TitleContainer = styled.div`
+        border: 2px dashed red;
+        margin-bottom: 25px;
+    `;
+
+    export const TodayNewsArticles = styled.div`
+        border: 2px solid white;
+        display: flex;
+        flex-direction: column;
+        
+    `;
+
+    export const FeaturedArticle = styled.div`
+        margin: 50px auto;
+        width: 90%;
+        height: auto;
+        border: 2px solid green;
+        padding: 30px;
+        display: flex;
+    `;
+
+    export const StandardArticlesContainer = styled.div`
+        display: flex;
+        flex-wrap: wrap;
+        border: 3px solid pink;
+    `;
+
+    export const StandardArticle = styled.div`
+        margin: 50px auto;
+        width: 350px;
+        display: flex;
+    `;
+
+    export const FeaturedImgWrapper = styled.div`
+        padding-left: 20px;
+        padding-right: 20px;
+    `;
+
+    export const StandardImgWrapper = styled.div`
+
+    `;
+
+    export const FeaturedImg = styled.img`
+        width: 450px;
+        height: 450px;
+        object-fit: cover;
+    `;
+
+    export const StandardImg = styled.img`
+        width: 350px;
+        height: auto;
+    `;
+
+    export const ArticleHeadingWrapper = styled.div`
+        border: 2px solid purple;
+        width: 400px;
+        background: #000;
+        padding: 25px;
+    `;
 
 // set type for todayNewsData that will be accessed as props
 interface MainProps {
@@ -10,30 +92,33 @@ interface MainProps {
 const TodayNews: React.FC<MainProps> = ({ todayNewsData }) => {
 
     return (
-        <div className="TodayNewsSection">
-            <div className="titleContainer">
+        <TodayNewsSection>
+            <TitleContainer>
                 <h1>Today</h1>
-            </div>
-            <div className="TodayNewsContent">
-                    {todayNewsData.status ?
+            </TitleContainer>
 
+                    {todayNewsData.status ?
+                            
                         // map through array and return jsx for each element
                         todayNewsData.articles.map((article, i) => {
-                            // console.log(article.url);
                             return (
-                                // display article image, and title
-                                <div key={i} className="article">
-                                    <div className="imgWrapper">
-                                        <img src={article.urlToImage} className="articleImg" alt={article.description}/>
-                                    </div>
-                                    <div className="articleHeadingWrapper">
-                                        <h2>{article.title}</h2>
-                                    </div>
-                                </div>
+                                <TodayNewsArticles>
+                                    {/* // display article image, and title, if first, then display as featured article */}
+
+                                    <div key={i} className={i === 0 ? "FeaturedArticle" : "StandardArticle"}>
+                                        <FeaturedImgWrapper>
+                                            <FeaturedImg src={article.urlToImage} className="articleImg" alt={article.description}/>
+                                        </FeaturedImgWrapper>
+                                        <ArticleHeadingWrapper>
+                                            <h2>{article.title}</h2>
+                                        </ArticleHeadingWrapper>
+                                    </div> 
+
+                                </TodayNewsArticles>
                             )
                         }) : <div>Loading...</div>}
-                </div>
-        </div>
+        </TodayNewsSection>
+        
     )
 }
 
