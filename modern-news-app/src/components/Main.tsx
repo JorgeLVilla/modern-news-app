cce7c28674aad58cf4191f0b59bbc2d3556c4c5a
lap343 <lap343@gmail.com>
2021-08-27 13:54:54 -0700

    feat(news-articles):add 3 defferent styles to the articles components and make the layout be three then one then two articles per line.

--- modern-news-app/src/components/Main.tsx
+++ modern-news-app/src/components/Main.tsx
@@ -1,28 +1,57 @@
 import React from "react";
+import styled from "styled-components";
 import { IEverythingNewsResponse } from "../models/Articles";
+import { OneArticle, OneHalfArticle, OneThirdArticle } from "../style/Articles.style"
 
 interface MainProps {
     newsData: IEverythingNewsResponse
 }
 
+const MainStyled = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+`
+
 const Main: React.FC<MainProps> = ({ newsData }) => {
 
+    let ArticleSize = OneThirdArticle
+    let articleIndex = 0
+
+    // Func to set they layout as repeating with a 3-1-2 article pattern
+    const articleLayout = () => {
+        console.log(articleIndex)
+        if(articleIndex === 4){
+            articleIndex++
+            ArticleSize = OneHalfArticle
+        } else if(articleIndex === 5){
+            articleIndex = 0
+            ArticleSize = OneHalfArticle
+        }else if(articleIndex === 3){
+            articleIndex++
+            ArticleSize = OneArticle
+        } else{
+            articleIndex++
+            ArticleSize = OneThirdArticle
+        }
+    }
+
     return(
-        <div>
+        <MainStyled>
             {newsData.status ? (
                 newsData.articles.map((article, index) => {
-                    return <div key={index}>
-                        <img src={article.urlToImage} alt={"hi"}/>
-                        <div>{article.title}</div>
-                        <div>{article.author}</div>
-                        <div>{article.description}</div>
-                        <a href={article.url} style={{backgroundColor: "white",}}>Click me to learn more!</a>
-                        <div>{article.publishedAt}</div>
-                        <div>{article.content}</div>
-                    </div>
+                    articleLayout()
+                    return <ArticleSize key={index}>
+                        <img src={article.urlToImage} alt="What the article is trying to explain"/>
+                        <div className="info-container">
+                            <a className="title" href={article.url}>{article.title}</a>
+                            <div>{article.author}</div>
+                        </div>
+                    </ArticleSize>
                 })
             ) : <div>Loading...</div>}
-        </div>
+        </MainStyled>
     )
 }
 
