f61b0aa4f53245878a5f0195d62c08fb4a554ea8
lap343 <lap343@gmail.com>
2021-08-30 16:12:18 -0700

    feat(app-title):add title and and icon

--- modern-news-app/src/style/NavBar.style.ts
+++ modern-news-app/src/style/NavBar.style.ts
@@ -2,7 +2,12 @@ import styled from 'styled-components';
 
 export const NavBarStyle = styled.nav`
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
     align-items: center;
     padding: 4em 2em;
+
+    & div{
+        padding-left: 1.2em;
+        font-size: 4em;
+    }
 `
