93ae462d84fa231fa1052f0c4e1e4a295384d8be
Jorge Villalobos <jorrgee.4l@gmail.com>
2021-09-03 11:20:51 -0700

    deleted a comment and formatted text

--- modern-news-app/src/components/TodayNews.tsx
+++ modern-news-app/src/components/TodayNews.tsx
@@ -1,100 +1,107 @@
-import React from 'react';
-import { ITodayNewsResponse } from '../models/TodayArticles';
+import React from "react";
+import { ITodayNewsResponse } from "../models/TodayArticles";
 import { OneArticle, OneThirdArticle } from "../style/Articles.style";
 import { MainPageTitle } from "../style/SectionTitles.style";
 
 // set type for todayNewsData that will be accessed as props
 interface MainProps {
-    todayNewsData: ITodayNewsResponse;
+  todayNewsData: ITodayNewsResponse;
 }
 
 // enable react props
 const TodayNews: React.FC<MainProps> = ({ todayNewsData }) => {
+  let ArticleSize = OneThirdArticle;
 
-    let ArticleSize = OneThirdArticle
-    // let articleIndex = 0
-        
-    // seperate TodayNews from the rest of articles stored in props
-    const returnFeaturedArticle = () => {
-        const article = todayNewsData.articles[0];
-        
-        return (
-            <>
-            {todayNewsData.status?
-            <OneArticle>
-            <ArticleSize>
-            <div className="img-block">
-                <img src={article.urlToImage} alt="What the article is trying to explain"/>
-                <span className="click-here">Click to see more</span>
-            </div>
-            <div className="info-container">
-                <div>
-                    <div className="title" >{article.title}</div>
-                    <div className="description">Description:<br/>{article.description}</div>
-                </div>
-                <div className="bottom-info">
-                    <div>Author: {article.author}</div>
-                    <div>From: {article.source.name}</div>
-                </div>
-            </div>
-        </ArticleSize>
-        </OneArticle>
-                : "Loading..."}
-            </>
-            )
-        }
-
-    // map through rest of articles with a different grouping
-    const returnStandardArticles = () => {
-        const articles = todayNewsData.articles.slice(1);
-
-        return (
-            <>
-            {
-            todayNewsData.status?  
-                articles.map((article, i) => {
-
-                    return (
-                        <OneThirdArticle>
-                        <ArticleSize>
-                                <div className="img-block">
-                                    <img src={article.urlToImage} alt="What the article is trying to explain"/>
-                                    <span className="click-here">Click to see more</span>
-                                </div>
-                                <div className="info-container">
-                                    <div>
-                                        <div className="title" >{article.title}</div>
-                                        <div className="description">Description:<br/>{article.description}</div>
-                                    </div>
-                                    <div className="bottom-info">
-                                        <div>Author: {article.author}</div>
-                                        <div>From: {article.source.name}</div>
-                                    </div>
-                                </div>
-                            </ArticleSize>
-                            </OneThirdArticle>
-                    )
-
-                }) 
-                : "Loading..."
-            }
-            </>
-        )
-    }    
+  // seperate TodayNews from the rest of articles stored in props
+  const returnFeaturedArticle = () => {
+    const article = todayNewsData.articles[0];
 
     return (
-        <>
-            <div>
-                <MainPageTitle>Today</MainPageTitle>
-            </div>
-            
-            {returnFeaturedArticle()}
-            {returnStandardArticles()}
+      <>
+        {todayNewsData.status ? (
+          <OneArticle>
+            <ArticleSize>
+              <div className="img-block">
+                <img
+                  src={article.urlToImage}
+                  alt="What the article is trying to explain"
+                />
+                <span className="click-here">Click to see more</span>
+              </div>
+              <div className="info-container">
+                <div>
+                  <div className="title">{article.title}</div>
+                  <div className="description">
+                    Description:
+                    <br />
+                    {article.description}
+                  </div>
+                </div>
+                <div className="bottom-info">
+                  <div>Author: {article.author}</div>
+                  <div>From: {article.source.name}</div>
+                </div>
+              </div>
+            </ArticleSize>
+          </OneArticle>
+        ) : (
+          "Loading..."
+        )}
+      </>
+    );
+  };
 
-        </>
-        
-    )
-}
+  // map through rest of articles with a different grouping
+  const returnStandardArticles = () => {
+    const articles = todayNewsData.articles.slice(1);
 
-export default TodayNews
+    return (
+      <>
+        {todayNewsData.status
+          ? articles.map((article, i) => {
+              return (
+                <OneThirdArticle>
+                  <ArticleSize>
+                    <div className="img-block">
+                      <img
+                        src={article.urlToImage}
+                        alt="What the article is trying to explain"
+                      />
+                      <span className="click-here">Click to see more</span>
+                    </div>
+                    <div className="info-container">
+                      <div>
+                        <div className="title">{article.title}</div>
+                        <div className="description">
+                          Description:
+                          <br />
+                          {article.description}
+                        </div>
+                      </div>
+                      <div className="bottom-info">
+                        <div>Author: {article.author}</div>
+                        <div>From: {article.source.name}</div>
+                      </div>
+                    </div>
+                  </ArticleSize>
+                </OneThirdArticle>
+              );
+            })
+          : "Loading..."}
+      </>
+    );
+  };
 
+  return (
+    <>
+      <div>
+        <MainPageTitle>Today</MainPageTitle>
+      </div>
+
+      {returnFeaturedArticle()}
+      {returnStandardArticles()}
+    </>
+  );
+};
+
+export default TodayNews;
--- modern-news-app/src/components/TopNews.tsx
+++ modern-news-app/src/components/TopNews.tsx
@@ -1,68 +1,78 @@
 import React from "react";
 import { IEverythingNewsResponse } from "../models/Articles";
 import { ArticlesStyled } from "./Main";
-import { OneArticle, OneHalfArticle, OneThirdArticle } from "../style/Articles.style";
+import {
+  OneArticle,
+  OneHalfArticle,
+  OneThirdArticle,
+} from "../style/Articles.style";
 import { MainPageTitle } from "../style/SectionTitles.style";
 
 interface TopNewsProps {
-    topNewsData: IEverythingNewsResponse;
+  topNewsData: IEverythingNewsResponse;
 }
 
 const TopNews: React.FC<TopNewsProps> = ({ topNewsData }) => {
-    
+  let ArticleSize = OneThirdArticle;
+  let articleIndex = 0;
 
-    let ArticleSize = OneThirdArticle
-    let articleIndex = 0
-
-    // Func to set they layout as repeating with a 3-1-2 article pattern
-    const articleLayout = ():void => {
-        if(articleIndex === 4){
-            articleIndex++
-            ArticleSize = OneArticle
-        } else if(articleIndex === 5){
-            articleIndex = 0
-            ArticleSize = OneHalfArticle
-        }else if(articleIndex === 3){
-            articleIndex++
-            ArticleSize = OneArticle
-        } else{
-            articleIndex++
-            ArticleSize = OneThirdArticle
-        }
+  // Func to set they layout as repeating with a 3-1-2 article pattern
+  const articleLayout = (): void => {
+    if (articleIndex === 4) {
+      articleIndex++;
+      ArticleSize = OneArticle;
+    } else if (articleIndex === 5) {
+      articleIndex = 0;
+      ArticleSize = OneHalfArticle;
+    } else if (articleIndex === 3) {
+      articleIndex++;
+      ArticleSize = OneArticle;
+    } else {
+      articleIndex++;
+      ArticleSize = OneThirdArticle;
     }
+  };
 
-    return (
-        <div>
-            <MainPageTitle>
-                Top News
-            </MainPageTitle>
-            <ArticlesStyled>
-                {topNewsData.status ?
-                    topNewsData.articles.map((article, i) => {
-                        articleLayout();
-                        return (
-
-                            <ArticleSize key={i}>
-                               <div className="img-block">
-                                    <img src={article.urlToImage} alt="What the article is trying to explain"/>
-                                    <span className="click-here">Click to see more</span>
-                                </div>
-                                <div className="info-container">
-                                    <div>
-                                        <div className="title" >{article.title}</div>
-                                        <div className="description">Description:<br/>{article.description}</div>
-                                    </div>
-                                    <div className="bottom-info">
-                                        <div>Author: {article.author}</div>
-                                        <div>From: {article.source.name}</div>
-                                    </div>
-                                </div>
-                            </ArticleSize>
-                        )
-                    }) : <div>Loading...</div>};
-            </ArticlesStyled>
-        </div>
-    )
-}
+  return (
+    <div>
+      <MainPageTitle>Top News</MainPageTitle>
+      <ArticlesStyled>
+        {topNewsData.status ? (
+          topNewsData.articles.map((article, i) => {
+            articleLayout();
+            return (
+              <ArticleSize key={i}>
+                <div className="img-block">
+                  <img
+                    src={article.urlToImage}
+                    alt="What the article is trying to explain"
+                  />
+                  <span className="click-here">Click to see more</span>
+                </div>
+                <div className="info-container">
+                  <div>
+                    <div className="title">{article.title}</div>
+                    <div className="description">
+                      Description:
+                      <br />
+                      {article.description}
+                    </div>
+                  </div>
+                  <div className="bottom-info">
+                    <div>Author: {article.author}</div>
+                    <div>From: {article.source.name}</div>
+                  </div>
+                </div>
+              </ArticleSize>
+            );
+          })
+        ) : (
+          <div>Loading...</div>
+        )}
+        ;
+      </ArticlesStyled>
+    </div>
+  );
+};
 
 export default TopNews;
